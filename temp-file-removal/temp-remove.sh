1c6af6691d69c53eeedf3b80a7706e600b89698a
jon8897 <jontykeefe8@gmail.com>
2023-05-02 14:24:30 +0100

    Fixing issue

--- temp-file-removal/temp-remove.sh
+++ temp-file-removal/temp-remove.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# This script removes all temporary files in the specified directory.
+# This script removes all temporary files in the specified directory and logs the names of deleted files.
 echo "Removing all temporary files..."
 
-# The 'find' command searches for all files with the '.tmp' extension in the specified directory and its subdirectories, and the '-delete' flag deletes them.
-find "/mnt/c/Users/AppData/Local/Temp" -type f -name "*.tmp" -delete
+# The 'find' command searches for all files with the '.tmp' extension in the 'Temp' directory of each drive on the PC, and the '-delete' flag deletes them. The 'tee' command writes the names of deleted files to a log file called 'deleted_files.log'.
+for drive in {c..f}; do
+  if [ -d "/mnt/$drive/Temp" ]; then
+    find "/mnt/$drive/Temp" -type f -name "*.tmp" -delete | tee -a deleted_files.log
+  fi
+done
 
-echo "Done!"
+echo "Done! Deleted files are logged in 'deleted_files.log'."
