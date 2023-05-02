2ce8388c80466cfa0cf680859da4182d929b369a
jon8897 <jontykeefe8@gmail.com>
2023-05-02 14:00:08 +0100

    Fixing issues

--- temp-file-removal/temp-remove.sh
+++ temp-file-removal/temp-remove.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script removes all temporary files in the current working directory.
+# This script removes all temporary files in the specified directory.
 echo "Removing all temporary files..."
 
-# The 'find' command searches for all files with the '.tmp' extension in the current directory and any subdirectories, and the '-delete' flag deletes them.
-find "/mnt/c/Users/AppData/Local/Temp" -type f -name "*.tmp" -delete
+# The 'find' command searches for all files with the '.tmp' extension in the specified directory and its subdirectories, and the '-delete' flag deletes them.
+find "/mnt/c/Users/jonathankeefe.ASTONBERKELEY/AppData/Local/Temp" -type f -name "*.tmp" -delete
 
 echo "Done!"
