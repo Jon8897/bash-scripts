d7b232c8688dbfa048b1d9ea5989c730e59f3654
jon8897 <jontykeefe8@gmail.com>
2023-05-05 15:05:11 +0100

    Making code universal

--- temp-file-removal/temp-remove.sh
+++ temp-file-removal/temp-remove.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# This script removes all temporary files in the specified directory and logs the names of deleted files.
+# This script removes all temporary files in the system's default temporary directory and logs the names of deleted files.
 echo "Removing all temporary files..."
 
-# The 'find' command searches for all files with the '.tmp' extension in the 'Temp' directory of each drive on the PC, and the '-delete' flag deletes them. The 'tee' command writes the names of deleted files to a log file called 'deleted_files.log'.
-for drive in {c..f}; do
-  if [ -d "/mnt/$drive/Temp" ]; then
-    find "/mnt/$drive/Temp" -type f -name "*.tmp" -delete | tee -a deleted_files.log
-  fi
-done
+# The 'find' command searches for all files with the '.tmp' extension in the default temporary directory and its subdirectories, and the '-delete' flag deletes them. The 'tee' command writes the names of deleted files to a log file called 'deleted_files.log'.
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  find "$TMPDIR" -type f -name "*.tmp" -delete | tee -a deleted_files.log
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  find "%TEMP%" -type f -name "*.tmp" -delete | tee -a deleted_files.log
+else
+  echo "Unsupported OS: $OSTYPE"
+  exit 1
+fi
 
 echo "Done! Deleted files are logged in 'deleted_files.log'."
